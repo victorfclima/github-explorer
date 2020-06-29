2b5524ede43f840fd9adc1dfd10ad03886a2f5fa
Victor França <victorfclima@gmail.com>
2020-06-29 14:08:24 -0300

    New: Add dashboard stylized interface

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,83 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositories at Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositories at Github</Title>
+      <Form action="">
+        <input type="text" placeholder="Enter repository name" />
+        <button type="submit">Search</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/63361486?s=460&u=3a441b8aabdb9b7d8afd66ac54d7498d100a0b73&v=4"
+            alt="Victor França"
+          />
+          <div>
+            <strong>Victorfclima/nodejs-api</strong>
+            <p>
+              [Monorep] API developed with NodeJS and Express, integrated with
+              React and React-Native via Axios. There is also a version using
+              TypeScript.
+            </p>
+          </div>
+          <FiChevronRight size={40} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/63361486?s=460&u=3a441b8aabdb9b7d8afd66ac54d7498d100a0b73&v=4"
+            alt="Victor França"
+          />
+          <div>
+            <strong>Victorfclima/nodejs-api</strong>
+            <p>
+              [Monorep] API developed with NodeJS and Express, integrated with
+              React and React-Native via Axios. There is also a version using
+              TypeScript.
+            </p>
+          </div>
+          <FiChevronRight size={40} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/63361486?s=460&u=3a441b8aabdb9b7d8afd66ac54d7498d100a0b73&v=4"
+            alt="Victor França"
+          />
+          <div>
+            <strong>Victorfclima/nodejs-api</strong>
+            <p>
+              [Monorep] API developed with NodeJS and Express, integrated with
+              React and React-Native via Axios. There is also a version using
+              TypeScript.
+            </p>
+          </div>
+          <FiChevronRight size={40} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/63361486?s=460&u=3a441b8aabdb9b7d8afd66ac54d7498d100a0b73&v=4"
+            alt="Victor França"
+          />
+          <div>
+            <strong>Victorfclima/nodejs-api</strong>
+            <p>
+              [Monorep] API developed with NodeJS and Express, integrated with
+              React and React-Native via Axios. There is also a version using
+              TypeScript.
+            </p>
+          </div>
+          <FiChevronRight size={40} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
